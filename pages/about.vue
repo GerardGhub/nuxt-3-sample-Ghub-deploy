f22d18d5681f48b749a354642685a996757f2861
GerardGhub <singian28@gmail.com>
2023-12-28 00:02:54 +0800

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur recusandae sunt labore, aperiam hic amet
+            pariatur odio saepe placeat, nesciunt quia exercitationem reiciendis. Cupiditate delectus distinctio quidem
+            vero, quasi numquam.
+        </p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}</style>
